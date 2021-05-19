2966f186558365fb5eec490f88ac61f006b35613
Julliot <bjulliot35@gmail.com>
2021-05-19 09:09:28 +0200

    New Controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Serie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,13 +23,43 @@ class MainController extends AbstractController
     /**
      * @Route("/test", name="main_test")
      */
-    public function test()
+    public function test(EntityManagerInterface $entityManager)
     {
-        $username = "<h1>Bastien</h1>";
-        $serie = ["title" => "Code Quantum", "year" => 1990];
-        return $this->render('main/test.html.twig',[
-            "user" => $username,
-            "serie" => $serie
+
+        $serie = new Serie();
+
+        $serie->setBackdrop('Ceci est un test')
+            ->setDateCreated(new \DateTime())
+            ->setFirstAirDate(new \DateTime("-1 year"))
+            ->setGenre("Western")
+            ->setLastAirDate(new \DateTime("-6 month"))
+            ->setName("Lucky Lucke")
+            ->setPopularity(100.8)
+            ->setPoster("TEST A NOUVEAU")
+            ->setStatus("Returning")
+            ->setTmdbId(123456)
+            ->setVote(9.8);
+
+        //autre possibilite d'init entity manager, soit ça, soit le truc dans la function
+        //$entityManager = $this->getDoctrine()->getManagers();
+
+        dump($serie);
+
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        $serie->setName("Calamity Jane");
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        dump($serie);
+
+        $entityManager->remove($serie);
+        $entityManager->flush();
+
+
+        return $this->render('main/test.html.twig', [
+
         ]);
     }
 }
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Serie;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+       $generator =  Faker\Factory::create("fr_FR");
+
+        for ($i = 0; $i <= 10; $i++){
+            $serie = new Serie();
+
+            $serie->setBackdrop($generator->imageUrl())
+                ->setDateCreated(new \DateTime())
+                ->setFirstAirDate(new \DateTime("-1 year"))
+                ->setGenre($generator->word())
+                ->setLastAirDate(new \DateTime("-6 month"))
+                ->setName($generator->word())
+                ->setPopularity(100.8)
+                ->setPoster($generator->imageUrl())
+                ->setStatus("Returning")
+                ->setTmdbId(123 + $i)
+                ->setVote($generator->randomFloat(2,1,10));
+
+            $manager->persist($serie);
+        }
+
+        $manager->flush();
+    }
+}
